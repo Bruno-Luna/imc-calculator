6bab9cba123245ee794dfb42f213fc1157d7e05c
Bruno Luna <luna.brsilva@gmail.com>
2021-08-27 22:44:05 -0300

    [refatorando JS. HTML mudanças nas tag.]

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,39 @@
 function calcularIMC(){
 
-        var formulario = document.getElementById('formulario');
-        var resultado = document.getElementById('res')
-        var kilos = parseInt(formulario.kilos.value);
-        // parseInt - metodo que transforma a string em um valor numérico
-        var metros = +formulario.metros.value;
-        //  o operador + vai forçar a conversão (somente em valor positivo)
-        var centimetros = +formulario.centimetros.value;
-   
-        
-        // alert (typeof(kilos)); exibe o tipo
-        var altura = (metros * 100 + centimetros)/100;
-        var imc = kilos / (altura * altura);
-
-        formulario.imc.value = imc.toFixed(2)
-        /*o método toFixed(2) apresenta o número 
-	   de casas decimais desejadas para o valor, nesse caso duas casas*/
+        var peso = parseInt(document.getElementById('peso').value);
+        var altura = parseFloat(document.getElementById('altura').value);
+         
+        var imc = peso / (altura * 2);
 
        if (imc <= 20){
-        resultado.innerText = 'Abaixo do Peso'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Abaixo do Peso'
+        res.style.color += '#b7094c'
        } else if( imc > 20 && imc <= 25){
-        resultado.innerText = 'Peso Ideal'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Peso Ideal'
+        res.style.color += '#b7094c'
        }else if( imc > 25 && imc <= 30){
-        resultado.innerText = 'Sobrepeso'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Sobrepeso'
+        res.style.color += '#b7094c'
        }else if( imc > 30 && imc <= 35){
-        resultado.innerText = 'Obesidade Moderada'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Obesidade Moderada'
+        res.style.color += '#b7094c'
        }else if( imc > 35 && imc <= 40){
-        resultado.innerText = 'Obesidade Severa'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Obesidade Severa'
+        res.style.color += '#b7094c'
        }else if( imc > 40 && imc <= 50){
-        resultado.innerText = 'Obesidade Mórbida'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Obesidade Mórbida'
+        res.style.color += '#b7094c'
        }else if ( imc > 50){
-        resultado.innerText = 'Super Obesidade'
-        resultado.style.color += '#b7094c'
+        numImc.innerText = imc.toFixed()
+        res.innerText = 'Super Obesidade'
+        res.style.color += '#b7094c'
        }else{
-           alert('Preencha todos os dados')
+           alert('Por favor, preencha todos os dados.')
        }
 }
