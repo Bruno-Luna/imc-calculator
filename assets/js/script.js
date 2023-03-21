6caabf73afc50865a0e34a80a97c8961508fee70
Bruno-Luna <luna.brsilva@gmail.com>
2023-03-20 22:11:33 -0300

    refactor: remoção de linhas duplicadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,42 +2,31 @@
 const botaoCalcular = document.querySelector('button')
 const form = document.querySelector('form')
 
-
 botaoCalcular.addEventListener(`click`, event => {
         event.preventDefault()
         verificarImc(form.kilos.value, form.altura.value)        
 })
 
-
 const verificarImc = (peso, altura) => {
+        if(!peso || !altura){
+            return alert('Por favor, preencha todos os dados.')
+        }
 
-        const imc = peso / (altura * 2);
+        const imc = peso / (altura * altura);
 
        if (imc <= 18.5){
-        numImc.innerText = imc.toFixed(1)
         res.innerText = 'Abaixo do Peso'
-        res.style.color += '#b7094c'
-       } else if( imc > 18.5 && imc <= 24.9){
-        numImc.innerText = imc.toFixed(1)
+       } else if(imc > 18.5 && imc <= 24.9){
         res.innerText = 'Peso Normal'
-        res.style.color += '#b7094c'
-       }else if( imc >= 25 && imc <= 29.9){
-        numImc.innerText = imc.toFixed(1)
+       }else if(imc >= 25 && imc <= 29.9){
         res.innerText = 'Sobrepeso'
-        res.style.color += '#b7094c'
-       }else if( imc >= 30 && imc <= 34.9){
-        numImc.innerText = imc.toFixed(1)
+       }else if(imc >= 30 && imc <= 34.9){
         res.innerText = 'Obesidade Grau I'
-        res.style.color += '#b7094c'
-       }else if( imc >= 35 && imc <= 39.9){
-        numImc.innerText = imc.toFixed(1)
+       }else if(imc >= 35 && imc <= 39.9){
         res.innerText = 'Obesidade Grau II'
-        res.style.color += '#b7094c'
-       }else if( imc >= 40){
-        numImc.innerText = imc.toFixed(1)
+       }else if(imc >= 40){
         res.innerText = 'Obesidade Mórbida'
-        res.style.color += '#b7094c'
-       }else{
-           alert('Por favor, preencha todos os dados.')
        }
+       numImc.innerText = imc.toFixed(1)
+       res.style.color += '#b7094c'
 }
