50d63576699307c661475a9533e390ad44f4434d
Bruno-Luna <luna.brsilva@gmail.com>
2023-04-11 19:34:27 -0300

    refactor: if/else dissolvidos

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,29 @@ botaoCalcular.addEventListener(`click`, event => {
 })
 
 const verificarImc = (peso, altura) => {
-        if(!peso || !altura){
-            return alert('Por favor, preencha todos os dados.')
-        }
-
-        const imc = peso / (altura * altura);
-
-       if (imc <= 18.5){
-        res.innerText = 'Abaixo do Peso'
-       } else if(imc > 18.5 && imc <= 24.9){
-        res.innerText = 'Peso Normal'
-       }else if(imc >= 25 && imc <= 29.9){
-        res.innerText = 'Sobrepeso'
-       }else if(imc >= 30 && imc <= 34.9){
-        res.innerText = 'Obesidade Grau I'
-       }else if(imc >= 35 && imc <= 39.9){
-        res.innerText = 'Obesidade Grau II'
-       }else if(imc >= 40){
-        res.innerText = 'Obesidade Mórbida'
-       }
-       numImc.innerText = imc.toFixed(1)
-       res.style.color += '#b7094c'
-}
+        if (!peso || !altura) return alert('Por favor, preencha todos os dados.');
+      
+        const imc = peso / altura ** 2;
+        const [limiar1, limiar2, limiar3, limiar4, limiar5] = [18.5, 24.9, 29.9, 34.9, 39.9];
+      
+        const mensagens = [
+          'Abaixo do Peso',
+          'Peso Normal',
+          'Sobrepeso',
+          'Obesidade Grau I',
+          'Obesidade Grau II',
+          'Obesidade Mórbida'
+        ];
+      
+        const classificacao = 
+            imc <= limiar1 ? 0
+          : imc <= limiar2 ? 1
+          : imc <= limiar3 ? 2
+          : imc <= limiar4 ? 3
+          : imc <= limiar5 ? 4
+          : 5;
+      
+        res.innerText = mensagens[classificacao];
+        numImc.innerText = imc.toFixed(1);
+        res.style.color = '#b7094c';
+      };
